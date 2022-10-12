a43f75ff385db2c317fb674954d3be09911ef5b0
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-12 17:42:59 -0300

    Created components by figma

--- labs-ds/src/App.tsx
+++ labs-ds/src/App.tsx
@@ -3,9 +3,9 @@ import './styles/global.css'
 export function App() {
   return ( 
     <>
-  <h1>Hello World</h1> 
+  <h1 className='text-2xl font-bold'>Hello World</h1> 
 
-  <button className='bg-violet-500 hover:bg-violet-900 font-medium px-4 py-2 rounded text-white '>Enviar</button>
+  <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300 '>Enviar</button>
   </>
   )
 }
