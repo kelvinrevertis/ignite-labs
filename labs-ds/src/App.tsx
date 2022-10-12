3d9c7cd87b2f1c0971d705e04f3a338c7b3cc212
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-12 14:34:07 -0300

    Import Inter font

--- labs-ds/src/App.tsx
+++ labs-ds/src/App.tsx
@@ -1,6 +1,12 @@
+import './styles/global.css'
+
 export function App() {
   return ( 
+    <>
   <h1>Hello World</h1> 
+
+  <button className='bg-violet-500 hover:bg-violet-900 font-medium px-4 py-2 rounded text-white '>Enviar</button>
+  </>
   )
 }
 
