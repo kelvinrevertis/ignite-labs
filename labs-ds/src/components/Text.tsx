cc10a23c411281aebb09b83a5917d6e67898cdb9
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-15 12:35:55 -0300

    Create Interface TextProps

--- labs-ds/src/components/Text.tsx
+++ labs-ds/src/components/Text.tsx
@@ -1,5 +1,10 @@
+interface TextProps{
+    size?:'sm'|'md'|'lg'
+}
+
+
 export function Text(){
     return(
-        <span className="text-gray-100">Hello World</span>
+        <span className="text-gray-100 font-sans">Hello World</span>
     )
 }
