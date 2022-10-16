d5eef13404d19bc0a75b89773cf71197ff0b98ba
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-10-16 20:38:23 -0300

    Install and using CLSX

--- labs-ds/src/components/Text.tsx
+++ labs-ds/src/components/Text.tsx
@@ -1,10 +1,20 @@
-interface TextProps{
-    size?:'sm'|'md'|'lg'
+import { clsx } from 'clsx'
+
+interface TextProps {
+    size?: 'sm' | 'md' | 'lg';
+    children: string;
 }
 
 
-export function Text(){
-    return(
-        <span className="text-gray-100 font-sans">Hello World</span>
+export function Text({ size = 'md' }: TextProps) {
+    return (
+        <span className={clsx("text-gray-100 font-sans",
+            {
+                'text-xs': size == 'sm',
+                'text-sm': size == 'md',
+                'text-md': size == 'lg',
+            })}>
+            Hello World
+        </span>
     )
 }
